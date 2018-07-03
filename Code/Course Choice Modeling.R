1e5c1b2287e59d00606e1fb3169c0eff1dfa86c6
wsmorgan <wsmorgan@asurite.asu.edu>
2018-07-03 13:42:13 -0700

    logging functionality and test run

--- Code/Course Choice Modeling.R
+++ Code/Course Choice Modeling.R
@@ -9,9 +9,9 @@
 # 0. Setup (libraries, paths, functions)
 # 1. Preprocessing (df --> matrix)
 # 2. Penalized Logistic Regression
-# 3. Support Vector Machine
+# 3. Support Vector Machine (RBF, Linear)
 # 4. Random Forest
-# 5. NN Classifier
+# 5. Boosted Classifier
 
 
 ###NOTES:
@@ -26,8 +26,7 @@
 ## 0. Setup
 rm(list = ls(all = TRUE))
 
-libs <- c("tidyverse", "glmnet", "e1071", "magrittr", "doParallel", "foreach",
-          'caret', 'randomForest')
+libs <- c("tidyverse", "magrittr", "doParallel", "foreach", 'caret')
 lapply(libs, library, character.only = TRUE)
 set.seed(18)
 
@@ -36,16 +35,16 @@ source("Code/CV_utils.R")
 
 # Start Log
 log <- file("Logs/Course Choice modeling.txt", open = "wt")
-sink(var_log, type = "output")
+cat("Course Choice Cross-Validation Result Log", file = log, sep = '\n')
 #------------------------------------------------------------------------------#
 
 ## 1. Import and Preprocessing
 training <- readRDS("Data/course choice training.Rds")
 validation <- readRDS("Data/course choice validation.Rds")
 
-## for testing purposes only:
-training %<>% sample_frac(0.01)
-validation %<>% sample_frac(0.01)
+# ## for testing purposes only:
+# training %<>% sample_frac(0.005)
+# validation %<>% sample_frac(0.005)
 
 # Create matrices for model estimation
 X_train <- model.matrix(icourse ~ ., training)
@@ -68,30 +67,57 @@ alpha_seq <-seq(0, 1, length.out = 11)
 logit_results <- cvLogit(X_train, Y_train, alpha_seq)
 
 # Print results for log
-cat("Logit Results:", "\n")
+cat("Logit Results:", file = log, sep = '\n')
 
 logit_results %>%
   arrange(misclassification) %>%
-  select(misclassification, alpha, lambda)
+  select(misclassification, alpha, lambda) %>%
+  mutate_all(function(x) round(x, 5)) %>%
+  head() %>%
+  write.table(., file = log, row.names = FALSE)
+
 #------------------------------------------------------------------------------#
 
-## 3. Support Vector Machine with RBF kernel
+## 3.1: Support Vector Machine with RBF kernel
 
-# Define param. grid
-svm_grid <- expand.grid(
-  cost = 10 ** runif(5, -3, 3),
-  gamma = 10 ** runif(5, -4, 1)
-)
-
-# Run CV
-svm_results <- cvSVM(X_train, Y_train, svm_grid, folds)
-
-# Print results for log
-cat("SVM Results:", "\n")
-
-svm_results %>%
-  arrange(misclassification) %>%
-  select(misclassification, gamma, cost)
+# # Define param. grid
+# svm_grid <- expand.grid(
+#   cost = 10 ** runif(5, -3, 3),
+#   gamma = 10 ** runif(5, -4, 1)
+# )
+# 
+# # Run CV
+# rbf_svm_results <- cvSVM(X_train, Y_train, kernel = 'rbf', svm_grid, folds)
+# 
+# # Print results for log
+# cat("RBF SVM Results:", file = log, sep = '\n')
+# 
+# rbf_svm_results %>%
+#   arrange(misclassification) %>%
+#   select(misclassification, gamma, cost) %>%
+#   mutate_all(function(x) round(x, 5)) %>%
+#   head() %>%
+#   write.table(., file = log, row.names = FALSE)
+# 
+# #------------------------------------------------------------------------------#
+# 
+# ## 3.2: SVM with Linear kernel
+# 
+# # Parameters
+# svm_grid <- expand.grid(cost = 10 ** runif(5, -3, 3))
+# 
+# # Run CV
+# lin_svm_results <- cvSVM(X_train, Y_train, kernel = 'linear', grid = g, folds)
+# 
+# # Print results
+# cat("Linear SVM Results:", file = log, sep = '\n')
+# 
+# lin_svm_results %>%
+#   arrange(misclassification) %>%
+#   select(misclassification, everything()) %>%
+#   mutate_all(function(x) round(x, 5)) %>%
+#   head() %>%
+#   write.table(., file = log, row.names = FALSE)
 #------------------------------------------------------------------------------#
 
 ## 4. Random Forests
@@ -108,10 +134,37 @@ rf_results <- foreach(i = 1:length(tree_sizes), .combine = bind_rows, .inorder =
 }
 
 # Print results
-cat("Random Forest Results:", "\n")
+cat("Random Forest Results:", file = log, sep = '\n')
 
 rf_results %>%
   arrange(misclassification) %>%
-  select(misclassification, everything())
+  select(misclassification, everything()) %>%
+  mutate_all(function(x) round(x, 5)) %>%
+  head() %>%
+  write.table(., file = log, row.names = FALSE)
 
-sink()
+#------------------------------------------------------------------------------#
+
+## 5. Boosted Classifier
+
+# Define param. grid
+boost_grid <- expand.grid(
+  depth = c(1, 4, 6, 10),
+  gamma = 10 ** runif(3, -2, 2)
+)
+
+# Run CV
+boost_results <- cvXGB(X_train, Y_train, boost_grid, folds)
+
+# Print results
+cat("Boosting Results:", file = log, sep = '\n')
+
+boost_results %>%
+  arrange(misclassification) %>%
+  select(misclassification, everything()) %>%
+  mutate_all(function(x) round(x, 5)) %>%
+  head() %>%
+  write.table(., file = log, row.names = FALSE)
+
+
+close(log)
