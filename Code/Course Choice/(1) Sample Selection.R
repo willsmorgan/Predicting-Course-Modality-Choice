7b01f23724dd3f949bc77db978887ab07a004626
wsmorgan <wsmorgan@asurite.asu.edu>
2018-07-17 16:59:21 -0700

    updated features and sample selection

--- Code/Course Choice/(1) Sample Selection.R
+++ Code/Course Choice/(1) Sample Selection.R
@@ -49,7 +49,7 @@ data <- fread("Data/full_data.csv")
 create_test_set = TRUE
 
 # Open Log
-#log <- file("Logs/Course Choice sample selection.txt", open = "wt")
+log <- file("Logs/Sample Selection.txt", open = "wt")
 write_lines('Course Choice Sample Selection Log', path = log)
 
 #------------------------------------------------------------------------------#
@@ -61,7 +61,7 @@ source("Code/Course Choice/(1A) Feature Creation.R")
 #------------------------------------------------------------------------------#
 ## 2. Initial drops
 
-cat("Initial number of observations: ", nrow(data), "\n", file = log)
+cat("\nInitial number of observations: ", nrow(data), "\n", file = log)
 
 data %<>%
   filter(strm >= 2114 & strm != 2149,
@@ -77,14 +77,15 @@ data %<>%
          nth_term <= 16,
          incoming_gpa <= 4.33,
          pell_eligibility != "",
-         ethnicity != "NR",
+         !ethnicity %in% c("NR", "Haw/Pac"),
          !is.na(first_gen),
          instruction_mode != "HY",
          crse_grade_off %in% c("A", "A-", "A+", "B", "B+", "B-",
                                "C", "C+", "C-", "D", "E", "W"),
-         session_code %in% c("A", "B", "C"))
+         session_code %in% c("A", "B", "C"),
+         !is.na(stem_degree))
 
-cat("Number of observations after initial drops: ", nrow(data), "\n", file = log)
+cat("\nNumber of observations after initial drops: ", nrow(data), "\n", file = log)
 
 #------------------------------------------------------------------------------#
 ## 3. Course Drops
@@ -98,7 +99,7 @@ data %<>% semi_join(., course_list, by = c("course", "strm"))
 
 rm(course_list)
 
-cat("Number of observations after selecting eligible courses: ", nrow(data), "\n", file = log)
+cat("\nNumber of observations after selecting eligible courses: ", nrow(data), "\n", file = log)
 
 #------------------------------------------------------------------------------#
 ## 4. Outliers, missing values, and factor variables
@@ -107,55 +108,59 @@ cat("Number of observations after selecting eligible courses: ", nrow(data), "\n
 ac <- c("Freshman", "sophomore", "Junior", "Senior","Post-Bacc Undergraduate", "Non-degree Undergraduate")
 cp <- c("TEMPE", "DTPHX", "POLY", "WEST")
 eth <- c("White", "American I", "2 or More", "Asian", "Black", "Haw/Pac", "Hispanic", "NR")
-sesh <- c("C", "B", "A")
 sterm <- c("2114", "2117", "2121", "2124", "2127", "2131", "2134", "2137",
           "2141", "2144", "2147", "2151", "2154", "2157", "2161", "2164",
           "2167", "2171", "2174", "2177")
+terms <- c("Fall", "Spring", "Summer")
+years <- c("2011", "2012",  "2013", "2014", "2015", "2016", "2017", "2018")
 
 # Factorize categorical variables and fix prev_term_gpa
 data %<>%
   mutate(prev_term_gpa = if_else(is.na(prev_term_gpa), incoming_gpa, prev_term_gpa),  # replace missing prev_term_gpa with incoming gpa for freshmen
+         prev_term_gpa2 = prev_term_gpa^2,
          acad_level = as_factor(acad_level, ac),
          campus = as_factor(campus, cp),
          ethnicity = as_factor(ethnicity, eth),
-         session = as_factor(session_code, sesh),
-         strm = as_factor(as.character(strm), sterm))
+         strm = as_factor(as.character(strm), sterm),
+         term = as_factor(term, terms),
+         school_year = as_factor(as.character(school_year), years))
 
 
 # Make sure indicator variables are factorized
 ind_vars <- c("icourse", "first_gen", "in_state", "gender", "pell_eligible", 
-              "required_course", "took_course", "took_instructor", "transfer",
-              "upper_division", "double_major")
+              "required_course", "stem_degree", "took_course", "took_instructor", "transfer",
+              "upper_division", "mult_degree", "include_in_gpa")
 
+# Edit indicators to all be 1/0, then force to factor setting reference at 0
 data %<>%
   mutate(pell_eligible = if_else(pell_eligibility == "Y", 1, 0),
          icourse = if_else(crse_modality == "ICOURSE", 1, 0),
-         gender = if_else(gender == "M", 1, 0)) %>%
+         gender = if_else(gender == "M", 1, 0),
+         include_in_gpa = if_else(include_in_gpa == "Y", 1, 0)) %>%
   mutate_at(vars(ind_vars), as.character) %>%
   mutate_at(vars(ind_vars), as_factor) %>%
   mutate_at(vars(ind_vars), function(x) relevel(x, "0"))
 
 # Define columns to keep
-cols <- c("acad_level", "age_at_class_start", "campus", "course_diff_index", 
-          "course_load", "cummean_grade_prev", "double_major", "dual_enrolled",
-          "ethnicity", "ever_pell", "fac_diff_index", "fed_efc", "first_gen", 
-          "gender", "icourse", "in_state", "incoming_gpa", "include_in_gpa", "nth_term", "num_ico_taken",
-          "pell_eligible", "prev_term_gpa", "prop_req_crses",
-          "required_course", "school_year", "stem_degree", "term", "tot_req_crses_bot",
-          "total_enrolled", "transfer", "trnsfr_credit_accpt")
+cols <- c("acad_level", "age", "age2", "campus", "cdi", "cdi2", 
+          "course_load", "ethnicity", "fed_efc", "first_gen", "gender", "icourse",
+          "in_state", "include_in_gpa", "mult_degree",  "nth_term", "num_ico_taken",
+          "pell_eligible", "prev_term_gpa", "prev_term_gpa2", "required_course", "stem_degree",
+          "tot_req_crses_bot", "total_enrolled", "transfer", "trf_creds_first_term",
+          "upper_division")
 
 # Drop observations with missing values in any column
 data %<>%
   select(one_of(cols)) %>%
   filter_all(all_vars(!is.na(.)))                        # HUGE sweep for all missing values
 
-cat("Number of observations after filtering on missing values: ", nrow(data), '\n', file = log)
+cat("\nNumber of observations after filtering on missing values: ", nrow(data), '\n', file = log)
 
 #------------------------------------------------------------------------------#
 ## 5. Variable Summary Log
 
-cat('\n\n\n', file = log)
-cat("PRE-TRIMMING SUMMARIES", '\n', file = log)
+cat('\n\n', file = log)
+cat("PRE-TRIMMING SUMMARIES\n", file = log)
 
 ## Make sure all factors have the reference level you want
 data %>% 
@@ -164,11 +169,11 @@ data %>%
 
 # Send summaries of variables to the log
 for (i in seq_along(data)){
-  if (is.numeric(data[, i])) {
-    cat(names(data)[i], file = log)
+  if (is.numeric(unlist(data[, i]))) {
+    cat("\n", file = log)
     capture.output(summary(data[, i]), file = log)
   } else {
-    cat(names(data)[i], file = log)
+    cat("\n", names(data)[i], file = log)
     capture.output(table(data[, i]), file = log)
   }
 }
@@ -183,20 +188,20 @@ for (i in seq_along(data)){
 data %<>%
   mutate(acad_level = relevel(acad_level, "Freshman"),
          campus = relevel(campus, "TEMPE")) %>%
-  filter(trnsfr_credit_accpt >= 0,
+  filter(trf_creds_first_term >= 0,
          fed_efc <= quantile(fed_efc, 0.95),
-         trnsfr_credit_accpt <= quantile(trnsfr_credit_accpt, 0.95)) %>%
+         trf_creds_first_term <= quantile(trf_creds_first_term, 0.95)) %>%
   select(order(colnames(.)))
 
 cat('\n\n\n', file = log)
 cat("POST-TRIMMING SUMMARIES", "\n", file = log)
 
 for (i in seq_along(data)){
-  if (is.numeric(data[, i])) {
-    cat(names(data)[i], file = log)
+  if (is.numeric(unlist(data[, i]))) {
+    cat("\n", file = log)
     capture.output(summary(data[, i]), file = log)
   } else {
-    cat(names(data)[i], file = log)
+    cat("\n", names(data)[i], file = log)
     capture.output(table(data[, i]), file = log)
   }
 }
--- Code/Course Choice/(1A) Feature Creation.R
+++ Code/Course Choice/(1A) Feature Creation.R
@@ -10,13 +10,12 @@
 ## Course Features:
 # Class size
 # Class level (lower division vs. upper division)
-# Cumulative average grade received in the course/modality combination 
+
 
 ## Student Features:
 # multiple majors
-# retention from previous term (are you more likely to take an icourse when you are returning from a break?)
 # number of iCourses previously taken
-# "STEM" degree - done by looking at CIP code of primary degree
+
 
 # NOTE: This script is not meant to be run independently from the sample selection
 # script. 
@@ -37,56 +36,25 @@ data %<>%
   mutate(total_enrolled = n()) %>%
   ungroup()
 
-avg_grades <- data %>%
-  select(course, crse_modality, strm, cgrade) %>%
-  filter(!is.na(cgrade)) %>%
-  group_by(course, crse_modality) %>%
-  arrange(strm) %>%
-  mutate(cummean_grade = cummean(cgrade)) %>%
-  group_by(course, crse_modality, strm) %>%
-  slice(1) %>%
-  group_by(course, crse_modality) %>%
-  mutate(cummean_grade_prev = lag(cummean_grade)) %>%
-  select(course, crse_modality, strm, cummean_grade_prev) %>%
-  ungroup()
-
-data %<>% inner_join(., avg_grades, by = c("course", "crse_modality", "strm"))
-
-rm(avg_grades)
 
 # Student Features (number of icourses taken, double major)
 print("Starting Student Features...")
 data %<>%
-  mutate(icourse = if_else(crse_modality == "ICOURSE", 1, 0)) %>%
+  mutate(icourse = if_else(crse_modality == "ICOURSE", 1, 0),
+         temp = paste0(acad_major_2, acad_cert_1, acad_minor_1),
+         mult_degree = if_else(acad_major_1 != "" & temp != "", 1, 0),
+         age2 = age_at_class_start^2,
+         cdi2 = course_diff_index^2) %>%
   group_by(emplid) %>%
   arrange(strm) %>%
-  mutate(double_major = if_else(acad_major_2 != "", 1, 0),
-         num_ico_taken = cumsum(icourse),
+  mutate(num_ico_taken = cumsum(icourse),
          num_ico_taken = lag(num_ico_taken)) %>%
   group_by(emplid, strm) %>%
   mutate(num_ico_taken = num_ico_taken[1],
          num_ico_taken = if_else(is.na(num_ico_taken), 0, num_ico_taken)) %>%
-  ungroup()
+  ungroup() %>%
+  select(-temp) %>%
+  rename(age = age_at_class_start,
+         cdi = course_diff_index)
   
-
-# THIS FEATURE IS TAKEN OUT BECAUSE IT DROPS TOO MANY OBSERVATIONS
-# # Student features (retained from previous term)
-# data %<>%
-#   group_by(emplid) %>%
-#   mutate(prev_observed_term = lag(strm)) %>%
-#   group_by(emplid, strm) %>%
-#   mutate(prev_observed_term = as.character(prev_observed_term[1]),
-#          prev_temp = str_sub(prev_observed_term, 4)) %>%
-#   ungroup() %>%
-#   mutate(prev_term_retention = case_when(
-#            term == "Spring" & prev_temp %in% c("4", "7")   ~ 1,
-#            term == "Summer" & prev_temp == "7"             ~ 1,
-#            term == "Fall" & prev_temp %in% c("1", "9")     ~ 1,
-#            term == "Winter" & prev_temp == "1"             ~ 0,
-#            graduation == 1 | is.na(prev_observed_term)     ~ NA_real_,
-#            TRUE                                            ~ NA_real_
-#          )) %>%
-#   select(
-#     -prev_temp) %>%
-#   ungroup()
 
